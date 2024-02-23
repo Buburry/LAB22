7029562f3265593921f2dd1f955fa83ec087d777
Buburry <Natapon7934@gmail.com>
2024-02-23 15:02:03 +0700

    22.1

--- average.cpp
+++ average.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+using namespace std;
 
+int main(int argc, char ** argv){
+
+    double avg;
+    double x[argc] = {};
+    if(argc == 1){
+        cout << "Please input numbers to find average.";
+    } else {
+        cout << "---------------------------------" << endl;
+        cout << "Average of " << argc-1 ;
+        for(int i = 0; i< argc; i++){
+            x[i] = atoi(argv[i]);
+
+        }
+        for(int i = 0; i< argc; i++){
+            avg = avg + x[i];
+
+        }
+        avg = avg/(argc-1);
+        cout << " numbers = " << avg << endl;
+        cout << "---------------------------------";
+    }
+    
+
+    return 0;
+}
